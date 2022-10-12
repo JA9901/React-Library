044123eb60be93f9e8c06f98fe04e3ef02854d6d
JA9901 <jareyes999@gmail.com>
2022-10-12 19:03:19 -0400

    added pages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 
 const Nav = () => {
+    function openMenu() {
+        document.body.classList += " menu--open";
+    }
+
+    function closeMenu() {
+        document.body.classList.remove("menu--open");
+    }
   return (
     <nav>
       <div className="nav__container">
@@ -10,28 +17,28 @@ const Nav = () => {
           <img src={LibraryLogo} alt="" className="logo" />
         </a>
         <ul className="nav__links">
-          <li className="">
+          <li className="nav__list">
             <a href="" className="nav__link">
               Home
             </a>
           </li>
-          <li className="">
+          <li className="nav__list">
             <a href="" className="nav__link">
               Books
             </a>
           </li>
-          <button className="btn__menu">
+          <button className="btn__menu" onClick={openMenu}>
             <FontAwesomeIcon icon="bars" />
           </button>
           <li className="nav__icon">
             <a href="/cart" className="nav__link">
               <FontAwesomeIcon icon="shopping-cart" />
             </a>
-            <span className="cart__length">3</span>
+            <span className="cart__length">2</span>
           </li>
         </ul>
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close">
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
             <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
