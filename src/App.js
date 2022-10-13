afaa3db5bcff0c80521c11be285c83df000f2ba2
JA9901 <jareyes999@gmail.com>
2022-10-13 03:50:49 -0400

    added cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 
 function App() {
@@ -12,7 +14,9 @@ function App() {
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route path="/books" render={() => <Books books={books} />}/>
+        <Route path="/books" exact render={() => <Books books={books} />}/>
+        <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+        <Route path="/cart" render={() => <Cart books={books} />} />
         <Footer />
       </div>
     </Router>
